5aebfdc499de2e059b9ae6d83ac8e40e632b1960
Ben Koala <bkoala123@hotmail.com>
2021-12-02 16:12:53 -0800

    Changes to css files

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/*import React from 'react';
 import { useLocation, useHistory } from 'react-router-dom';
 
 const Footer = () => {
@@ -18,6 +18,41 @@ const Footer = () => {
         <h4>
          UW bootcamp - Group - 6 [xxxxxxxxx]
         </h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+*/
+import React from 'react';
+import { useLocation, useHistory } from 'react-router-dom';
+
+const Footer = () => {
+  const location = useLocation();
+  const history = useHistory();
+  return (
+    <footer className="teal w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => history.goBack()}
+          >
+            &larr; Go Back
+          </button>
+        )}
+        <div class="">
+          <h4>
+            UW bootcamp - Group - 6 [xxxxxxxxx] Like what you see? Donate here <a class="btn-floating teal lighten-3 btn-large pulse"><i class="material-icons">attach_money</i></a>
+           
+             
+           </h4>
+           
+
+        </div> 
+        
+        
       </div>
     </footer>
   );
